7cf644c129b162dbb23419fccac396fc504cdfc6
feliperibeiro <felipe.ribeirosp@gmail.com>
2019-02-13 22:55:36 -0200

    Injeção de dependencias, Beans, Properties

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,12 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach{
 
+    @Autowired
     public FortuneService fortuneService;
 
-    @Autowired
-    public TennisCoach(FortuneService fortuneService){
-        this.fortuneService = fortuneService;
-    }
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
